74b0e2d106453067324e70cec91d9e665dc27ddf
edwardbryant <edward.bryant@gmail.com>
2015-01-29 17:12:00 -0600

    fixes

--- js/app.js
+++ js/app.js
@@ -29,7 +29,8 @@ function QuizQuestion(question_text,question_icon,possible_answers,correct_answe
 
 var quiz_questions = {
     1: {
-        "question": "In Army of Darkness, the main character travels through time with several objects, which of the below items is NOT one of these items?",
+        "icon": "book",
+        "question": "In the movie <strong>Army of Darkness (1992)</strong>, the main character travels through time with several objects, which of the below items is NOT one of these items?",
         "options": {
             1: "A twelve-gauge double-barreled Remington",
             2: "A chemistry text book",
@@ -40,6 +41,7 @@ var quiz_questions = {
         "answer": 3
     },
     2: {
+        "icon": "clock-o",
         "question": "Which of the below movies includes time travel as an element of the story?",
         "options": {
             1: "Once Upon a Time in America (1984)",
@@ -51,7 +53,8 @@ var quiz_questions = {
         "answer": 4
     },
     3: {
-        "question": "Which of the below movies is animated?",
+        "icon": "clock-o",
+        "question": "Which of the following time travel movies is animated?",
         "options": {
             1: "Galaxis (1995)",
             2: "The Girl Who Lept Through Time (2006)",
@@ -62,7 +65,8 @@ var quiz_questions = {
         "answer": 2
     },
     4: {
-        "question": "In Back to the Future, how many gigawatts of electricity does it take to travel in time?",
+        "icon": "clock-o",
+        "question": "In the movie <strong>Back to the Future (1985)</strong>, how many gigawatts of electricity does it take to travel in time?",
         "options": {
             1: "9.21",
             2: "1.81",
@@ -73,6 +77,7 @@ var quiz_questions = {
         "answer": 4
     },
     5: {
+        "icon": "male",
         "question": "Which of the following movies involves time travel without using a man-made device?",
         "options": {
             1: "About Time (2013)",
@@ -84,6 +89,7 @@ var quiz_questions = {
         "answer": 1
     },
     6: {
+        "icon": "ship",
         "question": "Which time travel movie involves a scene on the Titantic?",
         "options": {
             1: "Time After Time (1979)",
@@ -95,7 +101,8 @@ var quiz_questions = {
         "answer": 2
     },
     7: {
-        "question": "A Canadian 1989 time travel movie (originally titled, A Switch in Time) was re-titled to capitalize on the success of Bill &amp; Ted's Excellent Adventure. What was the movie's new title?",
+        "icon": "clock-o",
+        "question": "The Canadian movie <strong>A Switch in Time (1989)</strong> was re-titled to capitalize on the success of <strong>Bill &amp; Ted's Excellent Adventure (1989)</strong>. What was the movie's new title?",
         "options": {
             1: "Norman's Awesome Experience",
             2: "Jim &amp; Billy's Great Journey",
@@ -106,7 +113,8 @@ var quiz_questions = {
         "answer": 1
     },
     8: {
-        "question": "In the 2009 movie Frequently Asked Questions About Time Travel, a giant version of what appears in the future?",
+        "icon": "clock-o",
+        "question": "In the movie <strong>Frequently Asked Questions About Time Travel (2009)</strong>, a giant version of what appears in the future?",
         "options": {
             1: "gerbel",
             2: "ferret",
@@ -117,7 +125,8 @@ var quiz_questions = {
         "answer": 3
     },
     9: {
-        "question": "In the movie Back to the Future (1985), what was did the DeLorean's license plate read?",
+        "icon": "clock-o",
+        "question": "In the movie <strong>Back to the Future (1985)</strong>, what was did the DeLorean's license plate read?",
         "options": {
             1: "HGWELLS",
             2: "MCFLY1",
@@ -128,11 +137,12 @@ var quiz_questions = {
         "answer": 4
     },
     10: {
-        "question": "In Stephen King's novel 11/22/63, what event does the main character go back in time to try to prevent?",
+        "icon": "clock-o",
+        "question": "In Stephen King's novel <strong>11/22/63</strong>, what event does the main character go back in time to try to prevent?",
         "options": {
             1: "U.S. involvement in Vietnam",
             2: "The release of the Beatles first album",
-            3: "The assination of J.F.K.",
+            3: "The assasination of J.F.K.",
             4: "The Cuban Missle Crisis",
             5: "The Apollo moon landing"
         },
@@ -183,19 +193,15 @@ function wasAsked(num){
 }
 
 function loadQuestion(num) {
-    prior_questions.push(num);
-    
-    var question_text = quiz_questions[num]["question"];
-    var option_1 = quiz_questions[num]["options"][1];
-    var option_2 = quiz_questions[num]["options"][2];
-    var option_3 = quiz_questions[num]["options"][3];
-    var option_4 = quiz_questions[num]["options"][4];
-    var option_5 = quiz_questions[num]["options"][5];
-
-    $('#question > #text').text(question_text);
-
+    prior_questions.push(num);    
+    $('#icon').html("<i class=\"fa fa-"+quiz_questions[num]["icon"]+"\"></i>");
+    $('#text').html(quiz_questions[num]["question"]);
+    $('#option-1').html(quiz_questions[num]["options"][1]);
+    $('#option-2').html(quiz_questions[num]["options"][2]);
+    $('#option-3').html(quiz_questions[num]["options"][3]);
+    $('#option-4').html(quiz_questions[num]["options"][4]);
+    $('#option-5').html(quiz_questions[num]["options"][5]);
 }
-
 
 // document ready ...
 
@@ -205,22 +211,17 @@ $(document).ready(function(){
         var head = $("<span>The Time Travel Quiz</span>");
         $("h1").find("span").remove();
         $("h1").append(head);
-        $("#start").fadeOut(600);
-        
-
+        $("#start").fadeOut(600);        
         newGame();
-
         var num = findQuestion();
-
         loadQuestion(num);
-
-        
-
         $("#quiz").fadeIn(800);
-
-
     });
 
+    $("#answer-btn").click(function(){       
+        var num = findQuestion();
+        loadQuestion(num);
+    });
 
 
 
